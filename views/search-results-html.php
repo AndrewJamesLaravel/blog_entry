a4f046977757ceabd1bfe1a97768dbc74e56c6c2
andrew <win.cherchil@gmail.com>
2018-06-05 18:25:56 +0300

    6 commit

--- views/search-results-html.php
+++ views/search-results-html.php
@@ -5,8 +5,16 @@ if ( $searchDataFound === false ) {
     trigger_error("views/search-results-html.php needs $searchData");
 }
 
+$searchMessage = "";
+
 $searchHTML = "<section id='search'>
     <p>You searched for <em>$searchTerm</em></p><ul>";
+$rowCount = $searchData->rowCount();
+        if ( $rowCount === 0 ) {
+            $searchMessage = "<li>No entries match your search</li>";
+        }
+        $searchHTML .= $searchMessage;
+
 while ( $searchRow = $searchData->fetchObject() ) {
     $href = "index.php?page=blog&amp;id=$searchRow->entry_id";
     $searchHTML .= "<li><a href='$href'>$searchRow->title</a></li>";
